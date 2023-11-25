a1af36a59c563611f37574e24c019a99cc33ed03
Konstantyn Radchenko <radchenko1904@gmail.com>
2023-11-25 20:06:14 +0200

    markup task video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
+// _.throttle
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+const KEY_IFRAME_TIME = 'videoplayer-current-time';
+
+const onPlay = function ({ seconds }) {
+  const stringifyData = JSON.stringify(seconds);
+  localStorage.setItem(KEY_IFRAME_TIME, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(KEY_IFRAME_TIME)));
